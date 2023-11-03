a0ae498ac79d1bbd6fe1cf384e37648bb38db47c
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-11-03 22:03:39 +0100

    Contact Book - step 2 : done (add phone input and propTypes for the components)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,13 @@ import { nanoid } from 'nanoid';
 export class App extends Component {
   state = {
     contacts: [
-      { id: nanoid(), name: 'Rosie Simpson' },
-      { id: nanoid(), name: 'Hermione Kline' },
-      { id: nanoid(), name: 'Eden Clements' },
-      { id: nanoid(), name: 'Annie Copeland' },
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
     ],
     name: '',
+    number: '',
   };
 
 	addContact = (newContact) => {
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
 import css from './ContactList.module.css';
 
@@ -9,14 +10,24 @@ const ContactList = props => {
     <div className={css.contacts}>
       <h2>Contacts</h2>
       <ul className={css.contacts__list}>
-        {contacts.map(({ id, name }) => (
+        {contacts.map(({ id, name, number }) => (
           <li className={css.contacts__item} key={id}>
-            {name}
+            {name} : {number}
           </li>
         ))}
       </ul>
     </div>
   );
 };
+
+ContactList.propTypes = {
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      })
+    ),
+  };
 
 export default ContactList;
